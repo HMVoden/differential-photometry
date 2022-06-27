9b8f814941f04b18ad9823421134a888cddf42b9
Harlan Shaw <thatdamnpillow@gmail.com>
2022-06-27 12:22:16 -0600

    Modified click/black versions, changed accordion to treeview Made - button functional

--- src/shutterbug/ux/gui/main.py
+++ src/shutterbug/ux/gui/main.py
@@ -1,42 +1,27 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.popup import Popup
-from kivy.factory import Factory
-from kivy.properties import ObjectProperty, StringProperty
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.accordion import Accordion, AccordionItem
-from shutterbug.application import initialize_application, make_file_loader, make_dataset, make_reader_writer
+import logging
 from pathlib import Path
 
-from shutterbug.data.interfaces.external import Input
+from kivy.app import App
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.accordion import Accordion, AccordionItem
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.uix.treeview import TreeView, TreeViewNode, TreeViewLabel
+from shutterbug.application import initialize_application, make_file_loader
+
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
 
-class DatasetAccordion(Accordion):
-    dataset = ObjectProperty(None)
-    title = StringProperty(None)
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for loader in self.dataset:
-            self.add_widget(DatasetItem(title=self.title, loader=loader))
-
-class DatasetItem(AccordionItem):
-    loader = ObjectProperty(None)
-    view = ObjectProperty(None)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for name in self.loader.names:
-            self.view.add_widget(Label(text=name, height=20, size_hint_x=1, size_hint_y=None))
-
-
 
 class DatasetPanel(GridLayout):
     loadfile = ObjectProperty(None)
     scrollview = ObjectProperty(None)
+    treeview = ObjectProperty(None)
 
     def dismiss_popup(self):
         self._popup.dismiss()
@@ -48,9 +33,18 @@ class DatasetPanel(GridLayout):
 
     def load(self, path, filename):
         path = Path(filename[0])
-        loader = make_file_loader(path)
-        self.scrollview.add_widget(DatasetAccordion(title=path.stem, dataset=loader))
+        in_file = make_file_loader(path)
+        file_node = self.treeview.add_node(TreeViewLabel(text=path.stem))
+        for dataset in in_file:
+            for name in dataset.names:
+                self.treeview.add_node(TreeViewLabel(text=name), file_node)
         self.dismiss_popup()
+
+    def remove_dataset(self):
+        selected = self.treeview.selected_node
+        if isinstance(selected, TreeViewLabel) and (selected != self.treeview.root):
+            logging.info(f"Removing dataset {selected.text} from view")
+            self.treeview.remove_node(selected)
 
 
 class Root(FloatLayout):
@@ -62,13 +56,5 @@ class Main(App):
         config, database = initialize_application(debug=True)
 
 
-
-def run_gui():
-    Main().run()
-
-
-Factory.register("Root", cls=Root)
-Factory.register("LoadDialog", cls=LoadDialog)
-
 if __name__ == "__main__":
-    run_gui()
+    Main().run()
