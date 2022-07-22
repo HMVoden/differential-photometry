3b74812a7dd30ac93a112802e9a9bd550371a5c6
Harlan Voden <thatdamnpillow@gmail.com>
2022-07-22 16:09:51 -0600

    Added basic mainwindow code and runner to toml

--- src/shutterbug/ux/gui/main.py
+++ src/shutterbug/ux/gui/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+import sys
+from PySide6 import QtCore, QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+def run_gui():
+    app = QtWidgets.QApplication([])
+
+    main = MainWindow()
+    main.resize(800, 600)
+    main.show()
+
+    sys.exit(app.exec())
