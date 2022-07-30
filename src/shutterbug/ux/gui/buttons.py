2cb11ac5b58e48a1d9aff11c61602fcce597976e
Harlan Voden <thatdamnpillow@gmail.com>
2022-07-30 14:56:54 -0600

    Refined filebrowser, made uneditable list view

--- src/shutterbug/ux/gui/buttons.py
+++ src/shutterbug/ux/gui/buttons.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from PySide6.QtWidgets import QPushButton
-from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QPushButton, QWidget, QHBoxLayout
+from PySide6.QtGui import QIcon, Qt
 
 
 class IconButton(QPushButton):
@@ -14,4 +14,22 @@ class IconButton(QPushButton):
 
 
 class DropdownButton(QPushButton):
-    pass
+    def __init__(self):
+        QPushButton.__init__(self)
+
+
+class ButtonCluster(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+        self.layout.setSpacing(0)
+
+    def addWidget(
+        self,
+        arg__1: QWidget,
+        stretch: int = 0,
+        alignment: Qt.Alignment = Qt.Alignment(),
+    ):
+        self.layout.addWidget(arg__1, stretch, alignment)
--- src/shutterbug/ux/gui/file_browser.py
+++ src/shutterbug/ux/gui/file_browser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from PySide6 import QtWidgets
+from typing import List, Union, Optional
 from PySide6.QtWidgets import (
     QFileSystemModel,
     QListView,
@@ -10,11 +10,18 @@ from PySide6.QtWidgets import (
     QLineEdit,
     QFrame,
     QLabel,
+    QTreeView,
 )
-from PySide6.QtCore import QStandardPaths, QStorageInfo
+from PySide6.QtCore import (
+    QAbstractListModel,
+    QModelIndex,
+    QStandardPaths,
+    QStorageInfo,
+    Qt,
+)
 from PySide6.QtGui import QIcon, QPixmap
 from shutterbug.ux.gui import rc_icons
-from shutterbug.ux.gui.buttons import IconButton
+from shutterbug.ux.gui.buttons import DropdownButton, IconButton, ButtonCluster
 
 
 class FileBrowser(QWidget):
@@ -38,6 +45,7 @@ class FastNavigation(QWidget):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
         self.setMinimumWidth(200)
+        self.setMaximumWidth(200)
 
         self.layout.addWidget(StorageVolumes())
         self.layout.addWidget(SystemFolders())
@@ -69,22 +77,34 @@ class ButtonBar(QWidget):
         icon_fwd = QIcon(QPixmap(":/icons/arrow-right"))
         icon_up = QIcon(QPixmap(":/icons/long-arrow-left-up"))
         icon_refresh = QIcon(QPixmap(":/icons/refresh"))
-        self.layout.addWidget(IconButton(icon_back))
-        self.layout.addWidget(IconButton(icon_fwd))
-        self.layout.addWidget(IconButton(icon_up))
-        self.layout.addWidget(IconButton(icon_refresh))
+
+        nav_cluster = ButtonCluster()
+        self.nav_cluster = nav_cluster
+        nav_cluster.addWidget(IconButton(icon_back))
+        nav_cluster.addWidget(IconButton(icon_fwd))
+        nav_cluster.addWidget(IconButton(icon_up))
+        nav_cluster.addWidget(IconButton(icon_refresh))
+        self.layout.addWidget(nav_cluster)
 
         # Make Dir Group
         icon_add_dir = QIcon(QPixmap(":/icons/add-folder"))
         self.layout.addWidget(IconButton(icon_add_dir))
 
         # Directory and Search
-        self.layout.addWidget(QLineEdit())
-        self.layout.addWidget(QLineEdit())
+        self.directory_field = QLineEdit()
+        self.layout.addWidget(self.directory_field)
+        self.search_field = QLineEdit()
+        self.layout.addWidget(self.search_field)
 
         # Display Mode
 
         # Filter
+
+        filter_cluster = ButtonCluster()
+        icon_filter = QIcon(QPixmap(":/icons/filter"))
+
+        filter_cluster.addWidget(IconButton(icon_filter))
+        filter_cluster.addWidget(DropdownButton())
 
 
 class SaveBar(QWidget):
@@ -109,33 +129,56 @@ class FileView(QWidget):
         file_model = QFileSystemModel()
         default_place = QStandardPaths.standardLocations(QStandardPaths.HomeLocation)[0]
         file_model.setRootPath(default_place)
-        list_view = QListView()
-        list_view.setModel(file_model)
-        list_view.setRootIndex(file_model.index(file_model.rootPath()))
-        self.layout.addWidget(list_view)
+        view = QTreeView()
+        view.setItemsExpandable(False)
+
+        view.hideColumn(1)
+        view.setModel(file_model)
+        view.setRootIndex(file_model.index(file_model.rootPath()))
+        self.layout.addWidget(view)
         self.layout.addWidget(SaveBar())
+
+
+class StaticStringListModel(QAbstractListModel):
+    def __init__(self, data: List[str]):
+        QAbstractListModel.__init__(self)
+
+        self.m_itemData = data.copy()
+
+    def rowCount(self, parent: QModelIndex) -> int:
+        return len(self.m_itemData)
+
+    def data(self, index: QModelIndex, role: int) -> Optional[str]:
+        if index.isValid() and role == Qt.DisplayRole:
+            return self.m_itemData[index.row()]
+        else:
+            return None
 
 
 class StorageVolumes(CollapsibleGroup):
     def __init__(self):
         CollapsibleGroup.__init__(self, "Volumes")
 
-        for volume in QStorageInfo().mountedVolumes():
-            label = QLabel()
-            label.setText(volume.displayName())
-            self.layout.addWidget(label)
+        volumes = list(map(lambda x: x.displayName(), QStorageInfo.mountedVolumes()))
+
+        model = StaticStringListModel(volumes)
+        view = QListView()
+        view.setModel(model)
+
+        self.layout.addWidget(view)
 
 
 class SystemFolders(CollapsibleGroup):
     def __init__(self):
         CollapsibleGroup.__init__(self, "System")
 
-        self.layout.addWidget(QLabel("Home"))
-        self.layout.addWidget(QLabel("Desktop"))
-        self.layout.addWidget(QLabel("Documents"))
-        self.layout.addWidget(QLabel("Downloads"))
-        self.layout.addWidget(QLabel("Videos"))
-        self.layout.addWidget(QLabel("Music"))
+        sys_folders = ["Home", "Desktop", "Documents", "Downloads", "Videos", "Music"]
+
+        model = StaticStringListModel(sys_folders)
+        view = QListView()
+        view.setModel(model)
+
+        self.layout.addWidget(view)
 
 
 class Bookmarks(CollapsibleGroup):
@@ -143,3 +186,11 @@ class Bookmarks(CollapsibleGroup):
         CollapsibleGroup.__init__(self, "Bookmarks")
 
         self.layout.addWidget(QPushButton(text="Add Bookmark"))
+
+
+class Recents(CollapsibleGroup):
+    def __init__(self):
+        CollapsibleGroup.__init__(self, "Recent")
+
+    def get_most_recent(self) -> Union[None, str]:
+        return None
